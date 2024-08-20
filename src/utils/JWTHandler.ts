6786ff1794562ec8452851c737953b05d70d4a97
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-20 13:39:53 -0300

    feat: throws an error when not receiving a token as payload
    "TOKEN_NOT_FOUND" error

--- src/utils/JWTHandler.ts
+++ src/utils/JWTHandler.ts
@@ -1,19 +1,28 @@
 import jwt from 'jsonwebtoken'
 
-import { IGenerateTokenPayload } from '../interfaces'
-import { CustomError } from './Error'
+import { CustomError } from './error'
 import { EStatusCodes } from './../domain/statusCodes'
+import { IGenerateTokenPayload, IPerformJsonCallback } from '../interfaces'
 
 const { JWT_SECRET } = process.env
 
 export class JWTHandler {
   static generateToken = (payload: IGenerateTokenPayload) => jwt.sign(payload, JWT_SECRET ?? '', { expiresIn: '1d' })
 
-  static verifyToken = (token: string) => {
+  static verifyToken = (payload: { authorization: string }): IPerformJsonCallback<any> => {
+    if (!payload?.authorization) throw new CustomError('TOKEN_NOT_FOUND', EStatusCodes.UNAUTHORIZED)
+
     try {
+      const token = payload.authorization.split('Bearer ')[1]
+
       jwt.verify(token, JWT_SECRET ?? '')
+
+      return {
+        response: {},
+        status: EStatusCodes.OK
+      }
     } catch (tokenErr) {
-      throw new CustomError('Token error', EStatusCodes.UNAUTHORIZED)
+      throw new CustomError('TOKEN_ERROR', EStatusCodes.UNAUTHORIZED)
     }
   }
 }
