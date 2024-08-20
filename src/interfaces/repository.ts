26957f423b41e045bc7dd7e4b9b4e01183a57cda
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-20 13:38:38 -0300

    feat: create method to user.repository

--- src/interfaces/repository.ts
+++ src/interfaces/repository.ts
@@ -1,11 +1,10 @@
-import {
-  IUser
-} from '.'
+import { ISignupPayload, ISignupResponse, IUserInfoByEmailResponse } from '.'
 
 export interface IUserRepository {
-  getUserByEmail(email: string): Promise<IUser>
+  getUserByEmail(email: string): Promise<IUserInfoByEmailResponse>
+  create(payload: ISignupPayload): Promise<ISignupResponse>
 }
 
-export default class IRepositories {
+export default interface IRepositories {
   user: IUserRepository
 }
