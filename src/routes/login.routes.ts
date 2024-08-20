13fcd250664d62bb0370419a8d216858798b1a4f
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-20 13:39:09 -0300

    feat: signup route

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 
+import { ExpressAdapter } from '../adapters'
 import { LoginController } from '../controllers'
+import repositories from '../repositories'
 
 const router = express.Router()
 
-const loginController = new LoginController()
+const loginController = new LoginController(repositories)
 
-router.get('/login', loginController.login)
+router.post('/', ExpressAdapter.performJson(loginController.login))
+router.post('/signup', ExpressAdapter.performJson(loginController.signup))
 
 export default router
